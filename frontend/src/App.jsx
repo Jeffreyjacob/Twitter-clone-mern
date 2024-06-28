db2b0eeb218904a8387359c2dd23eff32dd6a8f8
Jeffreyjacob <jeffreyigiemeh16@gmail.com>
2024-06-28 04:52:41 -0700

    new commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import SignUpPage from "./pages/SignupPage"
+import LoginPage from "./pages/LoginPage"
+import HomePage from "./pages/home/HomePage"
+import Sidebar from "./components/common/Sidebar"
+import RightPanel from "./components/common/RightPanel"
+import NotificationPage from "./pages/notification/NotificationPage"
+import ProfilePage from "./pages/profile/ProfilePage"
 
 
 function App() {
   return (
-   <div>
-    hello world
+   <div className="flex max-w-6xl mx-auto">
+    <Sidebar/>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/signup' element={<SignUpPage/>}/>
+      <Route path="/notifications" element={<NotificationPage/>}/>
+      <Route path="/profile/:username" element={<ProfilePage/>}/>
+    </Routes>
+    <RightPanel/>
    </div>
   )
 }
